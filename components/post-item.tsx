5d8c9c81c832a29ff09ca08856b2c77bcf4c2921
Yogesh Singh <singhyogesh2104@gmail.com>
2024-08-31 21:11:54 +0530

    fix: added required component and delete unused component

--- components/post-item.tsx
+++ components/post-item.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import { Calendar, MoveRight } from "lucide-react";
 import Link from "next/link";
-import { buttonVariants } from "./ui/button";
+import { cva } from "class-variance-authority";
 
 interface PostProps{
     slug:string;
@@ -9,6 +9,37 @@ interface PostProps{
     description:string | undefined;
     date:string;
 }
+
+const buttonVariants = cva(
+    "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+    {
+      variants: {
+        variant: {
+          default: "bg-primary text-primary-foreground hover:bg-primary/90",
+          destructive:
+            "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          outline:
+            "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          secondary:
+            "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          ghost: "hover:bg-accent hover:text-accent-foreground",
+          link: "text-primary underline-offset-4 hover:underline",
+        },
+        size: {
+          default: "h-10 px-4 py-2",
+          sm: "h-9 rounded-md px-3",
+          lg: "h-11 rounded-md px-8",
+          icon: "h-10 w-10",
+        },
+      },
+      defaultVariants: {
+        variant: "default",
+        size: "default",
+      },
+    }
+  )
+
+  
 
 const BlogItem = ({slug,title,description,date}:PostProps) =>{
     const getDate = new Date(date);
